d1889a3473bdc066fcaa89aaa502cda1c3138c9e
Darren Butler <dnbutler64@gmail.com>
2021-03-24 18:16:45 +0000

    menus & gameplay countdown timer

---
+++ Assets/Scenes/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private static bool paused = false;
+    [SerializeField] private GameObject pauseMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(paused)
+            {
+
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        paused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        paused = false;
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scenes/Timer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float time = 10;
+    [SerializeField] private GameObject timerTextObject;
+    private bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameOver)
+        {
+            if (time > 0)
+            {
+                time -= Time.deltaTime;
+                DisplayTime(time);
+            }
+            else
+            {
+                time = -1;
+                gameOver = true;
+
+                DisplayTime(time);
+            }
+        }
+    }
+
+    private void DisplayTime(float time)
+    {
+        time++;
+
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+
+        timerTextObject.GetComponent<TextMeshProUGUI>().text = string.Format("Time: {0:00}:{1:00}", minutes, seconds);
+    }
+}
